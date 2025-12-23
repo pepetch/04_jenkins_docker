fd0f168fef9f188f7fbc26e141960c342620ba66
pepetch <pepetch0994749875>
2025-12-24 01:53:22 +0700

    Update page.js

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">suwanna aphakul 6604941</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
